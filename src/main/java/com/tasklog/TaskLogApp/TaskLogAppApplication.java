f4963fba524eeaaed7c3adabf9d26f4eccc61f67
Victor Sepulveda <victornsgr@gmail.com>
2022-07-29 20:56:27 -0400

    add base project

---
+++ src/main/java/com/tasklog/TaskLogApp/TaskLogAppApplication.java
@@ -0,0 +1,13 @@
+package com.tasklog.TaskLogApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskLogAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskLogAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tasklog/TaskLogApp/TaskLogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tasklog.TaskLogApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskLogAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
